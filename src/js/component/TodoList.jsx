efebc5e28410d499eb59691ef7208e5a667e50dc
Mariona Estrada <165200714+estradapm@users.noreply.github.com>
2024-06-20 10:39:19 +0000

    inicio

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
 
-
-const List = () => {
+const TodoList = () => {
     const [inputValue, setInputValue] = useState('');
-    const [task, setTask] = useState ([]);
-    
-    
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = (event) => {
+        if (event.key === 'Enter' && inputValue) {
+            setTasks([...tasks, inputValue]);
+            setInputValue('');
+        }
+    };
+
+    return (
+        <div>
+            <h1>TO DO LIST</h1>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyPress={addTask}
+            />
+            <ul>
+                {tasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
-export default TodoList; 
+export default TodoList;
